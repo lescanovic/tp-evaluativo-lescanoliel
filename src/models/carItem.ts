c86cfbaabbb7e9e30c838b8c59dca560b2a23bb9
lescanovic <victorialescanoarco@gmail.com>
2026-05-27 00:43:05 -0300

    feat: setup interfaces carItem, User and product

---
+++ src/models/carItem.ts
@@ -0,0 +1,5 @@
+import type { Product} from "./product";
+export interface carItem {
+    product: Product;
+    amount: number;
+}
---
+++ src/models/product.ts
@@ -0,0 +1,8 @@
+export interface Product {
+    id: number | string;
+    nombre: string;
+    precio: number;
+    descripcion: string;
+    descuento: number;
+    stock: number;
+}
